e1f3a0ec680749c735f054d0a7d616f6c60a8126
andrearnoni <andre_arnoni@yahoo.com.br>
2025-01-07 16:41:02 -0300

    implemented route pre-configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import "./input.css";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
