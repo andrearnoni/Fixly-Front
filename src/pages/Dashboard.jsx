092f0e7a9ebd9857da7626269440c4cf5d92ce12
andrearnoni <andre_arnoni@yahoo.com.br>
2025-02-21 16:55:21 -0300

    fixed style and responsive mode in graphics

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,16 +1,26 @@
-import { useContext } from "react";
+import { useState } from "react";
 import SideBar from "../components/SideBar";
 import Graphs from "../components/Graphs";
 
 function Dashboard() {
-    return (
-        <>
-            <div className="flex">
-                <SideBar />
-                <Graphs />
-            </div>
-        </>
-    )
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  return (
+    <div
+      className={`flex h-screen overflow-hidden ${
+        isDarkMode ? "bg-gray-900 text-white" : "bg-white text-black"
+      }`}
+    >
+      <SideBar toggleDarkMode={toggleDarkMode} isDarkMode={isDarkMode} />
+      <div className="flex-1">
+        <Graphs isDarkMode={isDarkMode} />
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
