2a5ae7eb5711942c88e0f165c4ce578e5935f471
victord-silva <victord.silva0206@gmail.com>
2025-03-07 00:05:57 -0300

    Atualização Services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,36 +1,48 @@
 import data from "../data/data.json"
-import { MessageCircle } from "lucide-react"
+import UserIcon from "../img/user.png"
+import { MessageCircle, ChevronsUp, ChevronsDown } from "lucide-react"
 
 
 const Services = () => {
     const profissionals = data
-    const actualDate = new Date().getFullYear()
     return (
-        <>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2 p-6">
-                {profissionals.map((profissionals, index) => (
-                    <div key={index} className="max-w-sm rounded overflow-hidden bg-white">
-                        <div className="px-6 py-4">
-                            <div className="font-bold text-lg text-center mb-2">
-                                {profissionals.slogan}
-                            </div>
-                            <p className="text-gray-700 text-xs">{profissionals.name},</p>
-                            <p className="text-gray-700 text-xs">{profissionals.category}</p>
-                            <p className="text-gray-700 text-xs">{profissionals.rate}</p>
-                            <p className="text-gray-700 text-xs">{profissionals.register}</p>
-                        </div>
-                        <div className="px-6 pt-4 pb-2">
-                            <button className="px-4 py-2">
-                                <MessageCircle className="text-blue-500 hover:text-blue-700"/>
-                            </button>
-                        </div>
-                        <div>
+        <article className="mt-10 px-6 md:px-8" id="services">
+            <div className="max-w-6xl mx-auto">
 
+                <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-2">
+                    
+                    {profissionals.map((profissionals, index) => (
+                        <div key={index} className="bg-white flex flex-col rounded shadow-lg hover:shadow-xl transition-all duration-300 hover:-translate-y-1 border border-gray-100 animate-fade-in">
+                            <div className="px-4 py-4 h-full flex flex-col justify-between">
+                                <div className="font-bold text-base line-clamp-none sm:line-clamp-3">
+                                    "{profissionals.descricao}"
+                                </div>
+                                <div className="grid grid-cols-2 text-center">
+                                    <p className="text-gray-700 text-sm col-span-2">{profissionals.nome},</p>
+                                    <p className="text-gray-700 text-sm font-bold flex flex-col items-center">
+                                        <ChevronsUp className="text-red-500" />
+                                        {profissionals.precoMaximo}</p>
+                                    <p className="text-gray-700 text-sm font-bold flex flex-col items-center">
+                                        <ChevronsDown className="text-blue-500" />
+                                        {profissionals.precoMinimo}
+                                    </p>
+                                    <p className="text-gray-700 text-sm">{profissionals.categoria}</p>
+                                    <p className="text-gray-700 text-sm">{profissionals.prestador}</p>
+                                </div>
+                            </div>
+                            <div className="px-6 pb-6 flex justify-around">
+                                <button type="button">
+                                    <img src={UserIcon} alt="Ícone de usuário" className="h-6 w-6" />
+                                </button>
+                                <button type="button">
+                                    <MessageCircle className="text-blue-500 hover:text-blue-700" />
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
-        </>
+        </article>
     )
 }
 
